0b008bf76880a644b8f07967b951041e9a856056
ben-altman <60247694+ben-altman@users.noreply.github.com>
2021-10-16 22:38:18 -0400

    commit tree 40fc6506fce34d57efde4bce3744eb77b33e3d66

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+};
+
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+};
+
+function wrapAdjective(sym = "*") {
+    return function(words = "special"){
+        return `You are ${sym}${words}${sym}!`
+    }
+};
+
+const Calculator = {
+    add: (num1, num2) => {
+        return num1 + num2
+    },
+    subtract: (num1, num2) => {
+        return num1 - num2
+    },
+
+    multiply: (num1, num2) => {
+        return num1 * num2
+    },
+
+    divide: (num1, num2) => {
+        return num1 / num2
+    }
+};
+
+function actionApplyer(int, arr) {
+    return arr.reduce(function(ans, element){return element(ans)}, int)
+}
